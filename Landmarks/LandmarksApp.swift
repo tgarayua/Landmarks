b4e9a61c60ad584462a95bcb6070c44886c1b7f1
Thomas Garayua <tgarayua@github.com>
2023-02-24 01:14:16 -0500

    second commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Thomas Garayua on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
